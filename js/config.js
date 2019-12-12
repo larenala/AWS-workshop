3d047c7e06929f221d6abea2d26b1fb7955d287d
larenala <laura.reivinen@gmail.com>
2019-12-12 17:28:14 +0000

    configure api invokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://0uzxrd3ug8.execute-api.eu-west-1.amazonaws.com/prod_student10' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
